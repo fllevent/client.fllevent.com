d9f54e41ca278d8c9fa552a9812318c6e73b0b84
Matt Behnke <matthewbehnke0@gmail.com>
2018-11-15 10:33:07 -0600

    updated tracking id

--- app/head.php
+++ app/head.php
@@ -22,14 +22,15 @@
       href="img/favicon.png">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109514313-2"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129296536-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'UA-109514313-2');
+      gtag('config', 'UA-129296536-1');
     </script>
+
     
   </head>
 
